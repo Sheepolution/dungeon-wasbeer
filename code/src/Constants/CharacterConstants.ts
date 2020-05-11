195220b717e5d9e8cdcb654ba86a0a5f479351ce
Sheepolution <danielhaazen@hotmail.com>
2020-05-11 20:04:46 +0200

    Change default values for characters.

--- code/src/Constants/CharacterConstants.ts
+++ code/src/Constants/CharacterConstants.ts
@@ -1,6 +1,6 @@
 export default class CharacterConstants {
 
-    public static readonly BASE_COOLDOWN_DURATION = 60;
+    public static readonly BASE_COOLDOWN_DURATION = 30;
 
     public static readonly HEAL_MESSAGE_AMOUNT = 10;
 
@@ -40,12 +40,12 @@ export default class CharacterConstants {
     }
 
     public static readonly CLASS_BASE_STATS = {
-        BARD: { armor: 0, charisma: 0, dexterity: 2, healing: 0, health: 100,  regeneration: 5, strength: 0, spell: 20, attack: 5 },
-        CLERIC: { armor: 0, charisma: 0, dexterity: 2, healing: 5, health: 100, regeneration: 1, strength: 0, spell: 1, attack: 5 },
-        WIZARD: { armor: 0, charisma: 0, dexterity: 1, healing: 0, health: 120, regeneration: 2, strength: 0, spell: 4, attack: 5 },
-        PALADIN: { armor: 5, charisma: 0, dexterity: 2, healing: 5, health: 100, regeneration: 1, strength: 20, spell: 0, attack: 5 },
-        FIGHTER: { armor: 2, charisma: 0, dexterity: 1, healing: 0, health: 100, regeneration: 1, strength: 5, spell: 0, attack: 5 },
-        RANGER: { armor: 0, charisma: 0, dexterity: 5, healing: 0, health: 100, regeneration: 1, strength: 1000, spell: 0, attack: 5 },
+        BARD: { armor: 0, charisma: 0, dexterity: 2, healing: 0, health: 100,  regeneration: 5, strength: 0, spell: 6, attack: 5 },
+        CLERIC: { armor: 0, charisma: 0, dexterity: 3, healing: 5, health: 100, regeneration: 1, strength: 0, spell: 3, attack: 5 },
+        WIZARD: { armor: 0, charisma: 0, dexterity: 1, healing: 0, health: 120, regeneration: 2, strength: 0, spell: 10, attack: 5 },
+        PALADIN: { armor: 5, charisma: 0, dexterity: 2, healing: 5, health: 100, regeneration: 1, strength: 3, spell: 0, attack: 2 },
+        FIGHTER: { armor: 2, charisma: 0, dexterity: 4, healing: 0, health: 90, regeneration: 2, strength: 5, spell: 0, attack: 5 },
+        RANGER: { armor: 0, charisma: 0, dexterity: 8, healing: 0, health: 100, regeneration: 1, strength: 1, spell: 0, attack: 5 },
     };
 
     public static readonly CLASS_ATTACK_MESSAGES = {
