4f54aa0d858b9c59daa9c2523b03684b04ea02c0
Sheepolution <danielhaazen@hotmail.com>
2020-05-16 13:02:52 +0200

    Add battle_id to xp logs.

--- code/src/Models/LogXPModel.ts
+++ code/src/Models/LogXPModel.ts
@@ -8,12 +8,13 @@ export default class LogXPModel extends Model {
         return 'logs_xp';
     }
 
-    public static async New(characterId:string, xp:number, dateString:string, trx:any) {
+    public static async New(battleId:string, characterId:string, xp:number, dateString:string, trx:any) {
         const logXPId = Utils.UUID();
 
         const log = await LogXPModel.query()
             .insert({
                 id: logXPId,
+                battle_id: battleId,
                 character_id: characterId,
                 xp: xp,
                 log_date: dateString,
--- code/src/Objects/LogXP.ts
+++ code/src/Objects/LogXP.ts
@@ -4,7 +4,7 @@ export default class LogXP {
 
     protected id:string;
 
-    public static async STATIC_POST(characterId:string, xp:number, dateString:string, trx:any) {
-        await LogXPModel.New(characterId, xp, dateString, trx);
+    public static async STATIC_POST(battleId:string, characterId:string, xp:number, dateString:string, trx:any) {
+        await LogXPModel.New(battleId, characterId, xp, dateString, trx);
     }
 }
