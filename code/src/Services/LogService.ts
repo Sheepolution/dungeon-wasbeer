a1c013f16dea2dfaea80b974775f3e10f0566a94
Sheepolution <danielhaazen@hotmail.com>
2020-05-20 22:46:05 +0200

    Change logs by moving ids to the start and removing date time.

--- code/src/Services/LogService.ts
+++ code/src/Services/LogService.ts
@@ -1,14 +1,13 @@
 import Player from '../Objects/Player';
 import { LogType } from '../Enums/LogType';
 import MessageService from './MessageService';
-import { Utils } from '../Utils/Utils';
 import Log from '../Objects/Log';
 import LogXP from '../Objects/LogXP';
 
 export default class LogService {
     public static async Log(player:Player, subjectId:string, logType:LogType, description:string) {
         await Log.STATIC_POST(player, subjectId, logType, description);
-        MessageService.SendMessageToLogChannel(`${Utils.GetNowString().slice(0, -5).replace('T', ' ')}: ${logType} - ${description} (${player.GetId()})`);
+        MessageService.SendMessageToLogChannel(`${player.GetId()} - ${logType} - ${description}`);
     }
 
     public static async LogXP(battleId:string, characterId:string, xp:number, dateString:string, trx:any) {
