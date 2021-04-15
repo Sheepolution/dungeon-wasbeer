2325ddfaf97c6558915b4db2846ca4fed901f124
Sheepolution <danielhaazen@hotmail.com>
2021-04-15 11:37:43 +0200

    Fix bug where sudokus can't be solved.

--- code/src/Handlers/CommandHandler.ts
+++ code/src/Handlers/CommandHandler.ts
@@ -52,6 +52,8 @@ export default class CommandHandler {
                 return;
             } else if (await BattleHandler.OnCommand(messageInfo, player, command)) {
                 return;
+            } else if (await PuzzleHandler.OnCommand(messageInfo, player, command, content)) {
+                return;
             }
         }
     }
